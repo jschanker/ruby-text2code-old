8745b861ac003844f414acbd347555722cc72e3f
Jason Schanker <jschanker@molloy.edu>
2015-12-23 22:46:26 -0500

    Experimenting more with metaprogramming

--- main.rb
+++ main.rb
@@ -5,6 +5,54 @@ require './text.rb'
 
 require './proxy.rb'
 
+class Variables
+	def method_missing(m, arr=nil)
+	end
+end
+
+@asd = "Y variable"
+puts local_variables
+
+Variable.x 425
+Variable.x.increase by 2
+puts Variable.x.to_s
+puts Variable.x.is divisible by 7
+
+class Food
+	self.attr_accessor :vf
+	@vf = 56
+	def self.foo n=nil
+		if(n == nil) then
+			puts "Foo function"
+			return @foo
+		end
+
+		@foo = n
+		return @foo
+	end
+
+	def self.foo= n
+		puts "FOOoOOO"
+		print self.vf
+		return @vf
+	end
+end
+
+#f = Food.new
+#puts f.foo "FOO VALUE"
+#puts f.foo
+#f.foo = "NO FOO VALUE"
+#puts f.foo
+print Food.foo = 125
+puts Food.foo
+puts Food.vf
+
+#puts Food.methods
+
+a = Mutablenum.new(4)
+a = a + 5
+print a
+
 #puts self.class
 let x = 45
 let y = 7
@@ -20,25 +68,27 @@ print length of s
 puts
 #d = set z to 15
 #set z to 25
-set z to 21
-z.set 54
-z.increase by 5
-z.decrease by 3
-z.multiply by 2
-z.divide by 2
+set z to 4
+#z = z + 3
+z = (z+3)
+#z.set 54
+#z.increase by 5
+#z.decrease by 3
+#z.multiply by 2
+#z.divide by 2
 puts
 puts
 #z = z + 5
 print z.class
 puts
 puts
-p z
-p z / 2
-p z
-puts "#{z}"
+#p z
+#p z / 2
+#p z
+#puts "#{z}"
 #puts "#{z.num}"
 #print self.class
-puts "#{z.to_i}"
+#puts "#{z.to_i}"
 #set z 15
 #set z to 80
 #set z to 100
--- variables.rb
+++ variables.rb
@@ -2,6 +2,21 @@
 
 #end
 
+class Variable
+	#set
+	def self.method_missing(m, *args)
+		if(args[0]) then
+			if(args[0].class == Fixnum) then
+				args[0] = Mutablenum.new(args[0])
+			end
+			instance_variable_set "@#{m}", args[0]
+			print args[0]
+		else
+			return instance_variable_get "@#{m}"
+		end
+	end
+end
+
 
 def let exp
 	# nothing to do here: allow ordinary assignment
