bd3ad46a0748ca155942e5a4edf9bfe05cca6a1a
jschanker <jschanker@molloy.edu>
2015-12-27 02:17:19 -0500

    More delegation to Variables class

--- proxy.rb
+++ proxy.rb
@@ -2,8 +2,36 @@ def set args
 	Variables.send(:set, args)
 end
 
+def increase inst
+	Variables.send(:increase, inst)
+end
+
+def decrease inst
+	Variables.send(:decrease, inst)
+end
+
+def multiply inst
+	Variables.send(:multiply, inst)
+end
+
+def divide inst
+	Variables.send(:divide, inst)
+end
+
 def to args
 	Variables.send(:to, args)
+end
+
+def by args
+	Variables.send(:by, args)
+end
+
+def is args
+	Variables.send(:is, args)
+end
+
+def divisible args
+	Variables.send(:divisible, args)
 end
 
 def method_missing(name, *args)
