f47cb7f42ce661c4aaf7b3af22e1ee2fb145e9fd
jschanker <jschanker@molloy.edu>
2015-12-26 22:56:42 -0500

    Start of set/get for variables

--- variables.rb
+++ variables.rb
@@ -1,26 +1,36 @@
-#class MutableNum
-
-#end
-
-class Variable
-	#set
-	def self.method_missing(m, *args)
-		if(args[0]) then
-			if(args[0].class == Fixnum) then
-				args[0] = Mutablenum.new(args[0])
-			end
-			instance_variable_set "@#{m}", args[0]
-			print args[0]
-		else
-			return instance_variable_get "@#{m}"
+class Variables
+	def self.set(args)
+		# arg[0] = var name, arg[1] = "to", arg[2] = value
+		if(args[2].class == Fixnum) then
+			instance_variable_set "@#{args[0]}", MutableNum.new(args[2])
+			self.send(:attr_accessor, "#{args[0]}")			
+			
+			return instance_variable_get "@#{args[0]}"
+			#MutableNum.send(:set, args)
 		end
 	end
-end
 
-
-def let exp
-	# nothing to do here: allow ordinary assignment
-	if exp.class != Fixnum then
-
+	def self.method_missing(name, *args)
+		# assume method name was variable
+		if(args.length > 0) then
+			if(args[0][0] == "to")
+				[name].concat(args[0])
+			end
+		else
+			return instance_variable_get "@#{name}"
+		end
 	end
+
+	def self.to(args)
+		#["to"] + [args]
+		["to"] + [args]
+	end
+'''
+	def createNewVariable(name, value)
+		instance_variable_set "@{name}", value
+		self.send(:attr_accessor, ":{name}")
+
+		return instance_variable_get "@{name}"
+	end
+'''
 end
