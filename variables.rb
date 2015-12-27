51f145be61a290a709d101fe27667b1ef98551e9
jschanker <jschanker@molloy.edu>
2015-12-27 02:19:50 -0500

    Additional functionality; incorporate instruction

--- variables.rb
+++ variables.rb
@@ -1,29 +1,60 @@
+require './instruction.rb'
+
 class Variables
-	def self.set(args)
+	def self.set(inst)
 		# arg[0] = var name, arg[1] = "to", arg[2] = value
-		if(args[2].class == Fixnum) then
-			instance_variable_set "@#{args[0]}", MutableNum.new(args[2])
-			self.send(:attr_accessor, "#{args[0]}")			
+		instance_variable_set "@#{inst.variable}", inst.values[0]
+		self.send(:attr_accessor, "#{inst.variable}")			
 			
-			return instance_variable_get "@#{args[0]}"
-			#MutableNum.send(:set, args)
-		end
+		return instance_variable_get "@#{inst.variable}"
+		#MutableNum.send(:set, args)
 	end
 
 	def self.method_missing(name, *args)
 		# assume method name was variable
-		if(args.length > 0) then
-			if(args[0][0] == "to")
-				[name].concat(args[0])
+		if(args[0] and args[0].class == Instruction) then
+			args[0].variable = name
+			if(args[0].action == "divisible") then
+				variable = instance_variable_get "@#{name}"
+				return variable.divisibleBy?(args[0].values[0])
 			end
+			return args[0]
 		else
 			return instance_variable_get "@#{name}"
 		end
 	end
 
-	def self.to(args)
-		#["to"] + [args]
-		["to"] + [args]
+	def self.to(val)
+		Instruction.new(val)
+	end
+
+	def self.by(val)
+		Instruction.new(val)
+	end
+
+	def self.is(inst)
+		inst
+	end
+
+	def self.increase(inst)
+		variable = instance_variable_get "@#{inst.variable}"
+		variable.send(:increase, inst.values[0])
+	end
+	def self.decrease(inst)
+		variable = instance_variable_get "@#{inst.variable}"
+		variable.send(:decrease, inst.values[0])
+	end
+	def self.multiply(inst)
+		variable = instance_variable_get "@#{inst.variable}"
+		variable.send(:multiply, inst.values[0])
+	end
+	def self.divide(inst)
+		variable = instance_variable_get "@#{inst.variable}"
+		variable.send(:divide, inst.values[0])
+	end
+	def self.divisible(inst)
+		inst.action = "divisible"
+		return inst
 	end
 '''
 	def createNewVariable(name, value)
