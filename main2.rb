a162f0506fc255145727e86a7c52470dca4f462e
jschanker <jschanker@molloy.edu>
2015-12-28 14:14:58 -0500

    Testing of functionality as of 12/27/15 - 2nd

--- main2.rb
+++ main2.rb
@@ -15,12 +15,34 @@ set d to d + 1
 puts d
 puts (50 + 2*d + 50)**2
 puts (d+1)**d
-set d to 20
+set d to -20 + 3
 
 puts d is divisible by d-(5+13)
 puts d is divisible by d*2
 puts d is divisible by 7
 puts d is divisible by 2
+puts d is odd
+
+set d to square root of 16
+puts 5^2
+puts d + 1
+puts square root of (d + 1)
+puts sqrt(d + 1)
+puts square root of 4 + 1
+puts Math.sqrt 4 + 1
+puts absolute -d
+
+if not d is prime then
+	puts "NEGATIVE"
+end
+
+if d^2 < 0 then
+	puts "POSITIVE"
+end
+
+puts 12.0 === 12
+
+#puts d+=1
 
 set abc to -425.5
 #puts abc is divisible by 5
@@ -31,3 +53,9 @@ set t to t * t + 5
 puts t
 puts *[10,5]
 puts 2.5.class
+
+a = 0
+if a < 5 then
+	a += 1
+	puts a
+end
