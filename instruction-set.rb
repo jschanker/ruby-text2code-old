4f9829742ad74878e8b29fc9964a05de81c2f7ee
jschanker <jschanker@molloy.edu>
2016-01-01 21:10:56 -0500

    More changes in progress

--- instruction-set.rb
+++ instruction-set.rb
@@ -1,12 +1,13 @@
 #require './instruction.rb'
 require 'set'
 require './math.rb'
+require './variables2.rb'
 
 module Text2Code
 
 	class Instruction
 		DEFAULT_VALUE = :EMPTY
-		attr_accessor :receiver
+		attr_accessor :receiver, :method_name, :class_receiver, :part_type
 		attr_reader :values, :actions
 
 		def initialize(*args)
@@ -18,10 +19,10 @@ module Text2Code
 				@receiver = args[0].receiver unless !args[0].receiver
 				args[0].values.each {|val| self.add_value(val)}
 				args[0].actions.each{|action| self.add_action(action)}
-				p self
+				#p self
 			else
 				args.each {|val| self.add_value(val)}
-				p @values
+				#p @values
 			end
 		end
 
@@ -45,6 +46,10 @@ module Text2Code
 
 		def same_values?(inst)
 			@values == inst.values
+		end
+
+		def has_same_form(inst)
+			self.same_actions?(inst) && !!@receiver == !!inst.receiver && @values.length == inst.values.length
 		end
 
 		def ==(inst)
@@ -75,8 +80,8 @@ module Text2Code
 	end
 
 	module InstructionCategory
-		VARIABLE = 1
-		MATH = 2
+		VARIABLE = Variables
+		MATH = MutableNum
 		TEXT = 3
 	end
 
@@ -98,8 +103,12 @@ module Text2Code
 					                                       instruction_type == InstructionPart::VARIABLE ||
 					                                       instruction_type == InstructionPart::VALUE
 					
-					@method_pairs[method_name].push(Instruction.new(instruction)) unless instruction_type == InstructionPart::VARIABLE ||
-					                                                    instruction_type == InstructionPart::VALUE
+					instruction_cp = Instruction.new(instruction)
+					instruction_cp.method_name = method if index == 0
+					instruction_cp.class_receiver = category unless instruction.receiver
+					instruction_cp.part_type = instruction_type
+					@method_pairs[method_name].push(instruction_cp) unless instruction_type == InstructionPart::VARIABLE ||
+					                                                       instruction_type == InstructionPart::VALUE
 
 					case instruction_type 
 					when InstructionPart::ACTION
@@ -116,29 +125,69 @@ module Text2Code
 			end
 		end
 
+		def self.method_missing(name, *args)
+			if(args[0] && args[0].class == Instruction) then
+				instruction = args[0]
+				instruction.receiver = name
+				if instruction.receiver && instruction.method_name then
+					return instruction.receiver.send(instruction.method_name, *instruction.values)
+				else
+					return instruction
+				end
+			elsif args[0]
+				return args[0]
+			else
+				InstructionCategory::VARIABLE.get(name)
+			end
+		end
+
 		def create_methods
 			@method_pairs.each do |method_name, instructions|
 				#alias_method
 				p method_name
 				p instructions
+				puts "---"
 				self.class.send(:define_method, method_name) do |*args|
+					# To be processed correctly, args should either be a 1-element array containing an instruction
+					# or a list of (non-Instruction) values
 					#p *args
-					inst = Instruction.new(*args)
+					ret_val = nil
+					instruction = Instruction.new(*args)
 					p instructions
 					p method_name
-					p inst
-					instructions.each do |instruction|
+					p instruction
+					instructions.each do |inst|
 						#p "INSTRUCTION:"
 						#p instruction
 						#p "INST: "
 						#p inst
 						#puts inst == instruction
 						#if(args[0])
-						if inst == instruction then
+						#if inst == instruction then
+						if inst.has_same_form(instruction) then
 							puts "FOUND MATCH"
 							p instruction
+							print "RET VALUE: "
+							p inst
+							case inst.part_type
+							when InstructionPart::ACTION
+								instruction.add_action(method_name)
+							end
+							ret_val = instruction
+							if inst.method_name then
+								puts "EXECUTING METHOD"
+								if instruction.receiver then
+									ret_val = instruction.receiver.send(inst.method_name, *instruction.values)
+								elsif inst.class_receiver then
+									ret_val = inst.class_receiver.send(inst.method_name, *instruction.values)
+								end
+							end
 						end
 					end
+
+					puts "**********"
+
+					ret_val # loop above should be rewritten as while; should break as soon as it finds a matching instruction
 				end
 			end
 		end
@@ -256,14 +305,6 @@ module Text2Code
 		                         {:var1 => InstructionPart::VARIABLE},
 		                         {:divided => InstructionPart::ACTION},
 		                         {:by => InstructionPart::PREPOSITION},
-		                         {:val1 => InstructionPart::VALUE}], :divided, 
-		                         InstructionCategory::MATH)
-
-	inst_set.add_instruction([{:remainder => InstructionPart::ACTION}, 
-		                         {:of => InstructionPart::PREPOSITION}, 
-		                         {:var1 => InstructionPart::VARIABLE},
-		                         {:divided => InstructionPart::ACTION},
-		                         {:by => InstructionPart::PREPOSITION},
 		                         {:val1 => InstructionPart::VALUE}], :remainder, 
 		                         InstructionCategory::MATH)
 
@@ -277,7 +318,7 @@ module Text2Code
 		                         {:val1 => InstructionPart::VALUE}], :abs, 
 		                         InstructionCategory::MATH)
 	inst_set.add_instruction([{:sqrt => InstructionPart::ACTION}, 
-		                         {:val1 => InstructionPart::VALUE}], :abs, 
+		                         {:val1 => InstructionPart::VALUE}], :sqrt, 
 		                         InstructionCategory::MATH)
 	inst_set.add_instruction([{:square => InstructionPart::ACTION},
 								 {:root => InstructionPart::ACTION},  
@@ -343,10 +384,14 @@ module Text2Code
 
 	inst_set.create_methods()
 	puts "Foo"
-	inst_set.to 1, 2, "abc"
-	my_inst = Instruction.new(5, 8)
-	my_inst.add_action("abc")
-	my_inst.receiver = "Foo"
-	inst_set.to my_inst
-	inst_set.sqrt :EMPTY
+	#inst_set.to 1, 2, "abc"
+	#my_inst = Instruction.new(5, 8)
+	#my_inst.add_action("abc")
+	#my_inst.receiver = "Foo"
+	#inst_set.to my_inst
+	#inst_set.sqrt :EMPTY
+	puts inst_set.sqrt(6.25)
+	puts inst_set.square(inst_set.root(inst_set.of(5)))
+	puts "HERE"
+	puts inst_set.square(inst_set.root(inst_set.of(MutableNum.new(0.0625))))
 end
