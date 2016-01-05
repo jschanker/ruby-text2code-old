f52946102f001a390016063c5b8840f96fb3a074
jschanker <jschanker@molloy.edu>
2016-01-05 01:30:31 -0500

    Implemented most functionality
    More cleanup still needed; need to restructure again

--- instruction-set.rb
+++ instruction-set.rb
@@ -3,6 +3,51 @@ require 'set'
 require './math.rb'
 require './variables2.rb'
 
+class Fixnum
+	def is(inst)
+		Text2Code::INSTRUCTION_SET.send(:is, inst)
+		Variables.set(self, :__free_integer__)
+		Text2Code::INSTRUCTION_SET.send(:__free_integer__, inst)
+	end
+end
+
+class String
+	def get(inst)
+		Text2Code::INSTRUCTION_SET.send(:get, inst)
+		Variables.set(self, :__free_string__)
+		Text2Code::INSTRUCTION_SET.send(:__free_string__, inst)
+	end
+	def find(inst)
+		if(inst.class == Text2Code::Instruction) then
+			inst = Text2Code::INSTRUCTION_SET.send(:find, inst)
+			Variables.set(self, :__free_string__)
+			Text2Code::INSTRUCTION_SET.send(:__free_string__, inst)
+		else
+			self.index(inst)+1
+		end
+	end
+	def self.length(str)
+		str.length
+	end
+	def char_at(index)
+		#index.class == MutableNum ? self[index.num-1..index.num-1] : self[index-1..index-1] # .. used for pre-1.9 versions
+		self.substring(index, index)
+	end
+	def substring(startIndex, endIndex)
+		self[startIndex-1..endIndex-1]
+	end
+	#def find(str)
+	#	self.index(str)+1
+	#end
+	def rfind(str)
+		self.rindex(str)+1
+	end
+	def self.prompt(str, inputType)
+		return inputType == :number ? gets.to_f : gets
+	end
+
+end
+
 module Text2Code
 
 	class Instruction
@@ -11,7 +56,6 @@ module Text2Code
 		attr_reader :values, :actions
 
 		def initialize(*args)
-			#p "Here"
 			@values = []
 			@actions = []
 
@@ -19,10 +63,8 @@ module Text2Code
 				@receiver = args[0].receiver unless !args[0].receiver
 				args[0].values.each {|val| self.add_value(val)}
 				args[0].actions.each{|action| self.add_action(action)}
-				#p self
 			else
 				args.each {|val| self.add_value(val)}
-				#p @values
 			end
 		end
 
@@ -77,12 +119,13 @@ module Text2Code
 		VERB = 5
 		NOUN = 6
 		TWO_VALUES = 7
+		ACTION_VALUE = 8
 	end
 
 	module InstructionCategory
 		VARIABLE = Variables
 		MATH = MutableNum
-		TEXT = 3
+		TEXT = String
 	end
 
 	class InstructionSet
@@ -119,6 +162,8 @@ module Text2Code
 							instruction_cp.add_action(method_name)
 						when InstructionPart::VALUE
 							instruction_cp.add_value
+						when InstructionPart::ACTION_VALUE
+							instruction_cp.add_value
 						when InstructionPart::TWO_VALUES
 							instruction_cp.add_value
 							instruction_cp.add_value
@@ -153,28 +198,21 @@ module Text2Code
 		end
 '''
 		def create_methods
-			puts "CREATE METHODS:"
-			p @method_pairs[:sqrt]
 			@method_pairs.each do |method_name, instructions|
 				#alias_method
-				p method_name
-				p instructions
-				puts "---"
 				self.class.send(:define_method, method_name) do |*args|
 					# To be processed correctly, args should either be a 1-element array containing an instruction
 					# or a list of (non-Instruction) values
-					#p *args
-					ret_val = nil
+					#ret_val = nil
 					variable_name = args.shift if method_name == :method_missing
 					instruction = Instruction.new(*args)
 
 					
 					if method_name == :method_missing then
+						puts "Returning with " if args.length == 0
+						Variables.get(variable_name) if args.length == 0
+						return Variables.get(variable_name) if args.length == 0
 						#variable_name = args[0]
-						puts "VN:"
-						p variable_name
-						p args[0]
-						puts "____________________________________________________________________________________________"
 						#variable_value = Variables.get(variable_name)
 						#if variable_name && variable_value then
 						#	instruction.receiver = variable_value
@@ -182,9 +220,7 @@ module Text2Code
 					end
 					
 					#instruction.receiver = Variables.get(variable_name) if variable_name && args.length == 0
-					p instructions
-					p method_name
-					p instruction
+					ret_val = instruction
 					instructions.each do |inst|
 						#p "INSTRUCTION:"
 						#p instruction
@@ -194,28 +230,24 @@ module Text2Code
 						#if(args[0])
 						#if inst == instruction then
 						if inst.has_same_form(instruction) then
-							puts "FOUND MATCH"
-							p instruction
-							print "RET VALUE: "
-							p inst
 							case inst.part_type
 							when InstructionPart::ACTION
 								instruction.add_action(method_name)
+							when InstructionPart::ACTION_VALUE
+								instruction.add_value(method_name)
 							when InstructionPart::VARIABLE
 								variable_value = Variables.get(variable_name)
-								if variable_name and variable_value then
-									puts "----------------"
-									p variable_name
-									puts "-----------------"
+								if variable_name && variable_value then
 									instruction.receiver = variable_value
 								else 
 									instruction.class_receiver = InstructionCategory::VARIABLE
 									instruction.add_value(variable_name) if variable_name
 								end
+								#instruction.add_value(variable_name) if variable_name
 							end
 							ret_val = instruction
+
 							if inst.method_name then
-								puts "EXECUTING METHOD"
 								if instruction.receiver then
 									ret_val = instruction.receiver.send(inst.method_name, *instruction.values)
 								elsif inst.class_receiver then
@@ -224,8 +256,6 @@ module Text2Code
 							end
 						end
 					end
-
-					puts "**********"
 
 					ret_val # loop above should be rewritten as while; should break as soon as it finds a matching instruction
 				end
@@ -288,11 +318,16 @@ module Text2Code
 
 	inst_set = InstructionSet.new()
 
-	inst_set.add_instruction([{:var1 => InstructionPart::VARIABLE}], :get, 
-		                         InstructionCategory::VARIABLE)
+	#inst_set.add_instruction([{:var1 => InstructionPart::VARIABLE}], :get, 
+	#	                         InstructionCategory::VARIABLE)
 
 	inst_set.add_instruction([{:set => InstructionPart::ACTION}, 
 		                         {:var1 => InstructionPart::VALUE}, 
+		                         {:to => InstructionPart::PREPOSITION},
+		                         {:val1 => InstructionPart::VALUE}], :set, 
+		                         InstructionCategory::VARIABLE)
+	inst_set.add_instruction([{:set => InstructionPart::ACTION}, 
+		                         {:var1 => InstructionPart::VARIABLE}, 
 		                         {:to => InstructionPart::PREPOSITION},
 		                         {:val1 => InstructionPart::VALUE}], :set, 
 		                         InstructionCategory::VARIABLE)
@@ -352,6 +387,7 @@ module Text2Code
 		                         InstructionCategory::MATH)
 
 	inst_set.add_instruction([{:var1 => InstructionPart::VARIABLE},
+				                 {:is => InstructionPart::PREPOSITION},
 		                         {:divisible => InstructionPart::ACTION},
 		                         {:by => InstructionPart::PREPOSITION},
 		                         {:val1 => InstructionPart::VALUE}], :divisible_by?, 
@@ -396,7 +432,8 @@ module Text2Code
 		                         {:first => InstructionPart::ACTION},
 		                         {:occurrence => InstructionPart::NOUN},
 		                         {:of => InstructionPart::PREPOSITION},
-		                         {:val => InstructionPart::VALUE}], :index, 
+		                         #{:val => InstructionPart::VALUE}], :index,
+		                         {:val => InstructionPart::VALUE}], :find,
 		                         InstructionCategory::TEXT)
 	inst_set.add_instruction([{:from => InstructionPart::PREPOSITION}, 
 		                         {:text => InstructionPart::NOUN}, 
@@ -405,13 +442,14 @@ module Text2Code
 		                         {:last => InstructionPart::ACTION},
 		                         {:occurrence => InstructionPart::NOUN},
 		                         {:of => InstructionPart::PREPOSITION},
-		                         {:val => InstructionPart::VALUE}], :rindex, 
+		                         #{:val => InstructionPart::VALUE}], :rindex,
+		                         {:val => InstructionPart::VALUE}], :rfind, 
 		                         InstructionCategory::TEXT)
 
 	inst_set.add_instruction([{:prompt => InstructionPart::ACTION}, 
 		                         {:to => InstructionPart::PREPOSITION}, 
 		                         {:get => InstructionPart::VERB},
-		                         {:text => InstructionPart::ACTION},
+		                         {:text => InstructionPart::ACTION_VALUE},
 		                         {:with => InstructionPart::PREPOSITION},
 		                         {:message => InstructionPart::ACTION},
 		                         {:val => InstructionPart::VALUE}], :prompt, 
@@ -419,7 +457,7 @@ module Text2Code
 	inst_set.add_instruction([{:prompt => InstructionPart::ACTION}, 
 		                         {:to => InstructionPart::PREPOSITION}, 
 		                         {:get => InstructionPart::VERB},
-		                         {:number => InstructionPart::ACTION},
+		                         {:number => InstructionPart::ACTION_VALUE},
 		                         {:with => InstructionPart::PREPOSITION},
 		                         {:message => InstructionPart::ACTION},
 		                         {:val => InstructionPart::VALUE}], :prompt, 
@@ -431,12 +469,13 @@ module Text2Code
 		#inst_set.send(:to, 5)
 		#p inst_set.my_methods
 		#puts args[0]
-		if args.length > 0 then
-			Text2Code::INSTRUCTION_SET.send(name, *args)
-		else
-			puts "M"
-			Variables.get(name)
-		end
+		#if args.length > 0 then
+		puts "WHAT:"
+		p args
+			return Text2Code::INSTRUCTION_SET.send(name, *args)
+		#else
+			#return Variables.get(name)
+		#end
 	end
 
 	inst_set.create_methods()
@@ -445,8 +484,45 @@ module Text2Code
 	#p inst_set.send(:to, 5)
 	#p square root of 8
 	#p absolute value of MutableNum.new(-230)
-	p set d to -230
-	increase d by 10
-	puts absolute of d
-	p d (is divisible by 5)
+	puts set d to -230
+	#p increase d by 10
+	#puts absolute of d
+	#p d is negative
+	#p (2*d).class
+	#set r to (2*d)
+	#puts r
+	#p (2*d).is divisible by (1*d)
+	puts set d to 68.5
+	divide d by 10
+	puts d
+
+	p set str to "abcdefgde"
+#str = "abcdefg"
+#puts "\n---"
+puts length of str
+puts from str get letter length of str
+puts from "abcdefgde".get letter ((length of str) - 2)
+puts length of str
+puts length of "abcdefgdef"
+puts from text str get substring from letter 3, (length of str)
+puts from text "abcdefgde".get substring from letter 3, (length of str)
+puts from text "abcdefgde".get substring from letter 3, 5 
+puts from text str find first occurrence of text "de"
+p from text "abcdefgde".find first occurrence of text "de"
+puts from text str find last occurrence of text "de"
+puts from text "abcdefgde".find last occurrence of text "de"
+puts str.index("d")
+puts "---"
+set a to 58
+set b to remainder of a divided by 10
+#p b
+puts remainder of a divided by b - 2
+#puts remainder of 25.divided by 10
+p (prompt to get number with message "Enter something interesting: ") + 1
+p (prompt to get text with message "Enter something interesting: ") + "yay"
+set number to remainder of a divided by 20
+puts "*******"
+puts number
+puts "*******"
+puts remainder of a divided by 10
 end
